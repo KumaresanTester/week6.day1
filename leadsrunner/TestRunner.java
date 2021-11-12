1becd35b39236961f8e145c135e4d766010b78fc
KumaresanTester <91654181+KumaresanTester@users.noreply.github.com>
2021-11-12 14:09:05 +0530

    Delete leadsrunner directory

--- leadsrunner/TestRunner.java
+++
@@ -1,9 +0,0 @@
-package leadsrunner;
-
-import io.cucumber.testng.AbstractTestNGCucumberTests;
-import io.cucumber.testng.CucumberOptions;
-
-@CucumberOptions(features = "src/test/java/leadsfeatures", glue = "leadssteps", monochrome = true, publish = true)
-public class TestRunner extends AbstractTestNGCucumberTests {
-
-}
